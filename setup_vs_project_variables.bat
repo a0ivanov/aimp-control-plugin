20ef8a17043908ffa80a8340e42ef7d74f35c63c
Alexey Ivanov <alexey0ivanov@gmail.com>
2011-04-29 01:28:11 +0400

    use only FREEIMAGELIB_DIR env var. FREEIMAGEPLUSLIB_DIR path will be relative to FREEIMAGELIB_DIR.

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -2,7 +2,6 @@
 @echo off
 
 set BOOST_DIR=c:\libraries\boost\boost_1_46_1
-set FREEIMAGELIB_DIR=c:\libraries\FreeImage
-set FREEIMAGEPLUSLIB_DIR=c:\libraries\FreeImage\Wrapper\FreeImagePlus
-::set AIMP_PLUGINS_DIR=c:\AIMP2\Plugins
-set AIMP_PLUGINS_DIR=c:\AIMP3\Plugins
+set FREEIMAGELIB_DIR=c:\libraries\FreeImage\3.15.0
+set AIMP_PLUGINS_DIR=c:\AIMP2\Plugins
+::set AIMP_PLUGINS_DIR=c:\AIMP3\Plugins
