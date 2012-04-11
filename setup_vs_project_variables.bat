fbd6ea2516b4756ca7198d2b42427b3c0bc84730
Alexey Ivanov <alexey0ivanov@gmail.com>
2012-04-12 02:50:55 +0400

    add "Program files" dir for test.

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -5,3 +5,4 @@ set BOOST_DIR=c:\libraries\boost\boost_1_48_0
 set FREEIMAGELIB_DIR=c:\libraries\FreeImage\3.15.0
 ::set AIMP_PLUGINS_DIR=c:\AIMP2\Plugins
 set AIMP_PLUGINS_DIR=c:\AIMP3\Plugins
+::set AIMP_PLUGINS_DIR=c:\Program Files\AIMP3\Plugins
