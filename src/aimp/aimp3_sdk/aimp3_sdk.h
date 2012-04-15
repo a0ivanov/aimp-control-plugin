9f45586f18a6fb72c3b075e00dc2b514cfdc253f
Alexey Ivanov <alexey0ivanov@gmail.com>
2012-04-15 16:50:44 +0400

    move InitGuid.h from AIMP3SDK namespace.

--- src/aimp/aimp3_sdk/aimp3_sdk.h
+++ src/aimp/aimp3_sdk/aimp3_sdk.h
@@ -1,10 +1,10 @@
 #ifndef AIMP3SDK_H
 #define AIMP3SDK_H
 
+#include <InitGuid.h> // need to force macro DEFINE_GUID to define guid instead declare.
+
 namespace AIMP3SDK
 {
-
-#include <InitGuid.h> // need to force macro DEFINE_GUID to define guid instead declare.
 
 // Add all AIMP SDK functionality to AIMP3SDK namespace.
 #include "AIMPSDKAddons.h"
