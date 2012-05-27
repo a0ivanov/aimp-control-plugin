cd2dbc99589e5cbdd4e921d444b663220c624265
Alexey Ivanov <alexey0ivanov@gmail.com>
2012-05-28 02:23:54 +0400

    use static linking instead dynamic of icu lib.

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -3,7 +3,8 @@
 set BOOST_DIR=c:\libraries\boost\boost_1_48_0
 ::set BOOST_DIR=c:\libraries\boost\boost_1_46_1
 set FREEIMAGELIB_DIR=c:\libraries\FreeImage\3.15.0
-set ICULIB_DIR=c:\libraries\icu\icu4c-49_1_1
+::set ICULIB_DIR=c:\libraries\icu\icu4c-49_1_1\prebuilt
+set ICULIB_DIR=c:\libraries\icu\icu4c-49_1_1\src
 ::set AIMP_PLUGINS_DIR=c:\AIMP2\Plugins
 set AIMP_PLUGINS_DIR=c:\AIMP3\Plugins
 ::set AIMP_PLUGINS_DIR=c:\Program Files\AIMP3\Plugins
