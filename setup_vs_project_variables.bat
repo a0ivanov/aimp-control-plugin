5a10e6ddd9d75faa2012e23882298bca64c59fcd
Alexey Ivanov <alexey0ivanov@gmail.com>
2012-11-17 21:17:29 +0400

    update libs:   - boost 1_48_0->1_49_0   - freeimage 3.15.0->3.15.3

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -1,8 +1,8 @@
 :: setup variables which are used in Visual Studio project.
 @echo off
-set BOOST_DIR=c:\libraries\boost\boost_1_48_0
+set BOOST_DIR=c:\libraries\boost\boost_1_49_0
 ::set BOOST_DIR=c:\libraries\boost\boost_1_46_1
-set FREEIMAGELIB_DIR=c:\libraries\FreeImage\3.15.0
+set FREEIMAGELIB_DIR=c:\libraries\FreeImage\3.15.3
 ::set AIMP_PLUGINS_DIR=c:\AIMP2\Plugins
 set AIMP_PLUGINS_DIR=c:\AIMP3\Plugins
 ::set AIMP_PLUGINS_DIR=c:\Program Files\AIMP3\Plugins
