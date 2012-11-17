3b98684202c66b91379439569b3e9c59dd1a2052
Alexey Ivanov <alexey0ivanov@gmail.com>
2012-11-17 21:18:52 +0400

    use x64 paths.

--- setup_environment.bat
+++ setup_environment.bat
@@ -1,20 +1,22 @@
 :: setup paths of all tools used in plugin build process.
 
 rem call "c:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-call "c:\Program Files\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
 
 :: java runtime is used by yui_compressor and google closure compiler in MINIMIZE_CLIENT_SRC task.
-set JAVA_HOME=%JAVA_HOME%
+rem set JAVA_HOME=%JAVA_HOME%
+set PATH=%PATH%;%JAVA_HOME%\bin
 
 set SUBWCREV_TOOL_DIR=c:\Program Files\TortoiseSVN\bin
-set INNO_SETUP_HOME=c:\Program Files\Inno Setup 5
+set INNO_SETUP_HOME=c:\Program Files (x86)\Inno Setup 5
 set PATH=%SUBWCREV_TOOL_DIR%;%INNO_SETUP_HOME%;%PATH%
 
 :: following variables are used for source indexing and storing debug symbols to symbol server.
-set PERL_DIR=c:\perl
-set DEBUG_TOOL_DIR=c:\Program Files\Debugging Tools for Windows (x86)
+set PERL_DIR=c:\Perl\ActivePerl\5.16.1.1601
+::c:\Program Files\Debugging Tools for Windows (x86)
+set DEBUG_TOOL_DIR=c:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86
 set SRCSRV_HOME=%DEBUG_TOOL_DIR%\srcsrv
-set SVN_CLIENT_DIR=c:\Program Files\Subversion\bin
+set SVN_CLIENT_DIR=c:\Program Files (x86)\Subversion\bin
 set PATH=%DEBUG_TOOL_DIR%;%SRCSRV_HOME%;%PERL_DIR%\bin;%SVN_CLIENT_DIR%\bin;%PATH%
 set SYMBOL_STORE=C:\Symbols
 
