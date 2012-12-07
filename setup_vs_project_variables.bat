63a30ebaf75bba884130cb0cc9a3ff5b41a452b2
Alexey Ivanov <alexey0ivanov@gmail.com>
2012-12-08 01:01:51 +0400

    use move to boost 1.52.0 instead 1.49.0

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -1,6 +1,6 @@
 :: setup variables which are used in Visual Studio project.
 @echo off
-set BOOST_DIR=c:\libraries\boost\boost_1_49_0
+set BOOST_DIR=c:\libraries\boost\boost_1_52_0
 ::set BOOST_DIR=c:\libraries\boost\boost_1_46_1
 set FREEIMAGELIB_DIR=c:\libraries\FreeImage\3.15.3
 ::set ICULIB_DIR=c:\libraries\icu\icu4c-49_1_1\prebuilt
