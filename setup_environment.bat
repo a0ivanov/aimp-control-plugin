88fe7f70b3379d56db9bdaea50bfa70575491018
Alexey Ivanov <alexey0ivanov@gmail.com>
2012-12-10 05:23:20 +0400

    use env variables for Program Files prefix. cosmetic

--- setup_environment.bat
+++ setup_environment.bat
@@ -2,25 +2,25 @@
 
 :: Avoid MSBuild's error MSB8008: Specified platform toolset (v110) is not installed or invalid.
 set VisualStudioVersion=11.0
-call "c:\Program Files (x86)\Microsoft Visual Studio %VisualStudioVersion%\VC\bin\vcvars32.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio %VisualStudioVersion%\VC\bin\vcvars32.bat"
 
 :: java runtime is used by yui_compressor and google closure compiler in MINIMIZE_CLIENT_SRC task.
 rem set JAVA_HOME=%JAVA_HOME%
 set PATH=%PATH%;%JAVA_HOME%\bin
 
-set SUBWCREV_TOOL_DIR=c:\Program Files\TortoiseSVN\bin
-set INNO_SETUP_HOME=c:\Program Files (x86)\Inno Setup 5
+set SUBWCREV_TOOL_DIR=%ProgramFiles%\TortoiseSVN\bin
+set INNO_SETUP_HOME=%ProgramFiles(x86)%\Inno Setup 5
 set PATH=%SUBWCREV_TOOL_DIR%;%INNO_SETUP_HOME%;%PATH%
 
 :: following variables are used for source indexing and storing debug symbols to symbol server.
 set PERL_DIR=c:\Perl\ActivePerl\5.16.1.1601
-::c:\Program Files\Debugging Tools for Windows (x86)
-set DEBUG_TOOL_DIR=c:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86
+:: Before windows 8 it was here: %ProgramFiles%\Debugging Tools for Windows (x86)
+set DEBUG_TOOL_DIR=%ProgramFiles(x86)%\Windows Kits\8.0\Debuggers\x86
 set SRCSRV_HOME=%DEBUG_TOOL_DIR%\srcsrv
-set SVN_CLIENT_DIR=c:\Program Files (x86)\Subversion\bin
+set SVN_CLIENT_DIR=%ProgramFiles(x86)%\Subversion\bin
 set PATH=%DEBUG_TOOL_DIR%;%SRCSRV_HOME%;%PERL_DIR%\bin;%SVN_CLIENT_DIR%\bin;%PATH%
 set SYMBOL_STORE=C:\Symbols
 
 :: following variables are used for documentation generation.
-set DOXYGEN_PATH=c:\Program Files\doxygen\bin
+set DOXYGEN_PATH=%ProgramFiles%\doxygen\bin
 set PATH=%DOXYGEN_PATH%;%PATH%
--- vs_launch.bat
+++ vs_launch.bat
@@ -5,6 +5,6 @@ call setup_vs_project_variables.bat > nul
 rem start msvc\aimp_control_plugin.sln
 
 :: This starts Visual Studio 2012 even if aimp_control_plugin.sln points to Visual C++ Express 2010
-::start "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\WDExpress" msvc\aimp_control_plugin.sln
+::start "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\Common7\IDE\WDExpress" msvc\aimp_control_plugin.sln
 
 start msvc\aimp_control_plugin.sln
