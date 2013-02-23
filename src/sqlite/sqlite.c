a61c4fbf539c95cdba8ec15dd12a9c71dfeaa095
Alexey Ivanov <alexey0ivanov@gmail.com>
2013-02-23 15:35:00 +0400

    Use dynamic linking with sqlite3.dll in AIMP root dir. Save 400KB of executable.

--- src/sqlite/sqlite.c
+++ src/sqlite/sqlite.c
@@ -3,6 +3,6 @@
 // This transaltion unit must not include sqlite3.h to avoid unresolved symbols due to they declared as external in "sqlite3.h".
 //#include "sqlite3/sqlite3.h" 
 
-#define _HAVE_SQLITE_CONFIG_H
-
-#include "sqlite3/sqlite3.c"
+// Use dynamic linking with sqlite3.dll in AIMP root dir.
+//#define _HAVE_SQLITE_CONFIG_H
+//#include "sqlite3/sqlite3.c"
--- src/sqlite/sqlite.h
+++ src/sqlite/sqlite.h
@@ -2,5 +2,7 @@
 
 #include "sqlite3/sqlite3.h"
 
-#define SQLITE_CORE
+// Use dynamic linking with sqlite3.dll in AIMP root dir.
+//#define SQLITE_CORE
+
 #include "sqlite3/sqlite3_unicode.h"
