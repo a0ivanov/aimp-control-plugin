8c9e7b8816b610578661d42ee5c22d4250b96e3f
Alexey Ivanov <alexey0ivanov@gmail.com>
2013-05-11 02:29:46 +0400

    fix grammar.

--- make_7z_package.bat
+++ make_7z_package.bat
@@ -7,8 +7,8 @@ call setup_environment.bat
 
 :: Notice that variables TEMP_BUILD_DIR and CLIENT_SRC_RELEASE_DIR are set in build_plugin.bat script.
 
-:CREATE_ARCHIEVE
-    echo Creating archieve...
+:CREATE_ARCHIVE
+    echo Creating archive...
     set /p PROJECT_VERSION= < %PROJECT_VERSION_FILE% || set PROJECT_VERSION=unknown_version
     set FULLPATH_ZIP=%TEMP_BUILD_DIR%\aimp_control_plugin_!PROJECT_VERSION!.7z
     
@@ -26,11 +26,11 @@ call setup_environment.bat
     copy /Y .\HowToSetupPluginFrom7zPackage.txt .\%TEMP_BUILD_DIR%\Readme.txt || goto ERROR_HANDLER 
     tools\7z a -t7z !FULLPATH_ZIP! ^
              .\%TEMP_BUILD_DIR%\Readme.txt || goto ERROR_HANDLER
-echo Archieve created.
+echo Archive created.
 exit /B
 
 
 :ERROR_HANDLER
     set ORIGINAL_ERROR_LEVEL=%ERRORLEVEL%
-    echo Archieve creation failed. Error code %ORIGINAL_ERROR_LEVEL%
+    echo Archive creation failed. Error code %ORIGINAL_ERROR_LEVEL%
     exit /B %ORIGINAL_ERROR_LEVEL%
