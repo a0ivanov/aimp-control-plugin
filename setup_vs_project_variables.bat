b52817ff38a33ac51309b9473b273b9aa54dca3c
Alexey Ivanov <alexey0ivanov@gmail.com>
2015-01-15 03:12:38 +0300

    use aimp 1468.

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -5,7 +5,7 @@ call setup_environment.bat > nul
 set BOOST_DIR=c:\libraries\boost\boost_1_57_0
 set FREEIMAGELIB_DIR=%~dp0\3rd_party\FreeImage\%FreeImage_VERSION%
 ::set AIMP_PLUGINS_DIR=%ProgramFiles%\AIMP3\Plugins
-set AIMP_PLUGINS_DIR=c:\AIMP\AIMP3.60.1466_beta\Plugins
+set AIMP_PLUGINS_DIR=c:\AIMP\AIMP3.60.1468_beta\Plugins
 ::for API 3.55- it should be
 ::  set AIMP_CONTROL_SUBPATH=\
 set AIMP_CONTROL_SUBPATH=\Control Plugin
