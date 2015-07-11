17fa196fb95d1d1d31fd6c519cccec75fb50a6c1
_y_s <ys0115sp@gmail.com>
2015-07-11 13:10:22 +0900

    Create Japanese file

---
+++ client_src/i18n/ja/aimp_manager.js
@@ -0,0 +1,36 @@
+/*
+    File contains localization for AimpManager web-interface.
+    Copyright (c) 2012, Alexey Ivanov
+*/
+var language_aimp_manager = {
+error_unknown : '不明なエラー',
+error_request_parsing : 'リクエストの解析に失敗しました',
+error_method_not_found : 'メソッドが見つかりません',
+error_type : 'タイプエラー',
+error_index_range : '範囲外のインデックス',
+error_object_access : 'オブジェクトが存在しません',
+error_value_range : '無効な値',
+error_internal : '内部プラグインエラー',
+error_wrong_argument : '無効な値',
+error_playback_failed : '再生に失敗しました',
+error_shuffle_mode_set_failed : 'シャッフル設定に失敗しました',
+error_repeat_mode_set_failed : 'リピート設定に失敗しました',
+error_volume_out_of_range : '音量が範囲外です',
+error_volume_set_failed : '音量の設定に失敗しました',
+error_mute_set_failed : 'ミュート設定に失敗しました',
+error_enqueue_track_failed : 'キューに追加できませんでした',
+error_dequeue_track_failed : 'キューから削除できませんでした',
+error_playlist_not_found : 'プレイリストが見つかりません',
+error_track_not_found : 'トラックが見つかりません',
+error_album_cover_load_failed : 'アルバムアートの読み込みに失敗しました',
+error_rating_set_failed : 'レーティングに失敗しました',
+error_status_set_failed : 'ステータス設定に失敗しました',
+error_radio_capture_set_failed : 'ラジオキャプチャ設定に失敗しました',
+error_move_track_in_queue_failed: 'キューの編集に失敗しました',
+error_add_url_to_playlist_failed: 'プレイリストにURLを追加できませんでした',
+error_remove_track_failed: 'トラックの削除に失敗しました。理由：トラックが見つかりません',
+error_remove_track_physical_deletion_disabled: 'トラックの削除に失敗しました。理由：プラグイン側の設定で無効にされています',
+error_scheduler_disabled: 'スケジューリングに失敗しました。理由：プラグイン側の設定で無効にされています',
+error_scheduler_unsupported_action: 'スケジューリングに失敗しました。理由：機器が対応していません。 プラグイン側の設定で無効にされている可能性があります。',
+error_playlist_creation_failed: 'プレイリストの作成に失敗しました'
+}
---
+++ client_src/i18n/ja/main_page.js
@@ -0,0 +1,92 @@
+/*
+    File contains localization for AimpManager web-interface.
+    Copyright (c) 2012, Alexey Ivanov
+*/
+var language_main_page = {
+
+page_title : 'AIMPリモートコントローラー',
+error_playlists_loading : 'プレイリストを取得できませんでした',
+error_playlist_entries_loading : 'トラックを取得できませんでした',
+error_stop_playback : '再生を停止できませんでした',
+error_pause_playback : '一時停止できませんでした',
+error_play_previous_track : '前のトラックを再生できませんでした',
+error_play_next_track : '次のトラックを再生できませんでした',
+error_volume_get : '音量を取得できませんでした',
+error_volume_set : '音量を設定できませんでした',
+error_get_control_panel_state : 'AIMPコントロールパネルを取得できませんでした',
+
+reason : '理由',
+reason_unknown : '不明な理由',
+playback_state_playing : '再生中',
+playback_state_stopped : '停止しました',
+kilobits_per_second : 'kbit/s',
+
+control_panel_playback : '再生',
+control_panel_pause : '一時停止',
+control_panel_stop : '再生を停止',
+control_panel_previous : '前のトラック',
+control_panel_next : '次のトラック',
+control_panel_shuffle : 'シャッフル',
+control_panel_shuffle_on : 'シャッフルをオン',
+control_panel_shuffle_off : 'シャッフルをオフ',
+control_panel_repeat : 'リピート',
+control_panel_repeat_on : 'リピートをオン',
+control_panel_repeat_off : 'リピートをオフ',
+control_panel_radio_capture_on: 'ラジオキャプチャをオン',
+control_panel_radio_capture_off: 'ラジオキャプチャをオフ',
+control_panel_radio_capture_disabled: 'ラジオキャプチャはインターネットラジオを再生中のとき利用可能です',
+control_panel_settings : '設定',
+control_panel_volume : '音量',
+control_panel_mute : 'ミュート',
+control_panel_mute_on : 'ミュートする',
+control_panel_mute_off : 'ミュートを解除',
+
+settings_dialog_title : '設定',
+settings_dialog_save_button : '設定を保存',
+settings_dialog_cancel_button : 'キャンセル',
+// tab 'playlist view'
+settings_dialog_playlist_view_tab_title : 'プレイリストビュー',
+settings_dialog_playlist_view_tab_show_fields : '表示する情報',
+settings_dialog_playlist_view_tab_hide_fields : '表示しない情報',
+settings_dialog_playlist_view_tab_entry_field_warning : '少なくとも1つの情報を表示する必要があります',
+// tab 'language'
+settings_dialog_language_tab_title : '言語',
+// tab track info
+settings_dialog_trackinfo_tab_title : 'トラック情報パネル',
+settings_dialog_trackinfo_format : 'トラック情報パネルフォーマット',
+
+entry_field_caption_title : '曲名',
+entry_field_caption_artist : 'アーティスト',
+entry_field_caption_album : 'アルバム',
+entry_field_caption_date : '年',
+entry_field_caption_genre : 'ジャンル',
+entry_field_caption_bitrate : 'ビットレート',
+entry_field_caption_duration : '曲の長さ',
+entry_field_caption_filesize : 'ファイルサイズ',
+entry_field_caption_rating : 'レーティング',
+entry_field_caption_albumcover : 'アルバムアート',
+
+track_contol_menu_open : 'トラックコントロールパネルを開く',
+track_contol_menu_close : 'トラックコントロールパネルを閉じる',
+track_contol_menu_play : '再生',
+track_contol_menu_pause : '一時停止',
+track_contol_menu_enqueue : 'キューに追加',
+track_contol_menu_remove_from_queue : 'キューから削除',
+track_contol_menu_download_track : 'ダウンロード',
+
+track_info_dialog_caption : 'トラック情報',
+track_info_dialog_cancel_rating : '未評価',
+
+playlist_contol_menu_add : 'プレイリストにアイテムを追加',
+playlist_contol_menu_add_disabled : 'AIMP Control Pluginの設定によりプレイリストに追加できません',
+playlist_contol_menu_add_not_supported : 'プラグインが対応していないためプレイリストに追加できません。プラグインを最新バージョンに更新してください。',
+playlist_contol_menu_item_add_file : 'ファイル',
+playlist_contol_menu_item_add_url : 'URL',
+playlist_contol_dialog_file_add_title: 'ファイルを追加',
+playlist_contol_dialog_file_add_message: 'ファイルを選択するか、ここにドラッグ&ドロップしてください',
+playlist_contol_dialog_file_add_button_caption: 'ファイルを追加',
+playlist_contol_dialog_url_add_title: 'URLを追加',
+playlist_contol_dialog_url_add_button_title: '追加',
+
+all_entries : 'すべて'
+}
