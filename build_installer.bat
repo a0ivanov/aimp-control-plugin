34a0338c8b23bf869a9447aae7b8c1305918ee66
Alexey Ivanov <alexey0ivanov@gmail.com>
2015-12-29 03:46:24 +0300

    skip signing of plugin DLL and installer.

--- build_installer.bat
+++ build_installer.bat
@@ -15,11 +15,12 @@ set PLUGIN_DLL=%TEMP_BUILD_DIR%\Control Plugin.dll
     call tools\plugin_dll_attributes_update.bat "%PLUGIN_DLL%" !PROJECT_VERSION! || goto ERROR_HANDLER
 
 :SIGN_PLUGIN_DLL
-    echo Signing plugin DLL...
-    call tools\codesign\sign_executable.bat "%PLUGIN_DLL%"
-    if ERRORLEVEL 1 (
-        goto ERROR_HANDLER
-    )
+      echo Skip signing of plugin DLL because cert is not available.
+::    echo Signing plugin DLL...
+::    call tools\codesign\sign_executable.bat "%PLUGIN_DLL%"
+::    if ERRORLEVEL 1 (
+::        goto ERROR_HANDLER
+::    )
     
 :SAVE_DEBUG_SYMBOLS
     call tools\save_debug_symbols.bat || goto ERROR_HANDLER
@@ -29,12 +30,13 @@ set PLUGIN_DLL=%TEMP_BUILD_DIR%\Control Plugin.dll
     ISCC inno_setup_data\aimp_control_plugin.iss || goto ERROR_HANDLER
 
 :SIGN_INSTALLER
-    echo Signing installer...
-    set INSTALLER_FILE=temp_build\Release\distrib\aimp_control_plugin-!PROJECT_VERSION!-setup.exe
-    call tools\codesign\sign_executable.bat %INSTALLER_FILE%
-    if ERRORLEVEL 1 (
-        goto ERROR_HANDLER
-    )
+      echo Skip signing of plugin installer because cert is not available.
+::    echo Signing installer...
+::    set INSTALLER_FILE=temp_build\Release\distrib\aimp_control_plugin-!PROJECT_VERSION!-setup.exe
+::    call tools\codesign\sign_executable.bat %INSTALLER_FILE%
+::    if ERRORLEVEL 1 (
+::        goto ERROR_HANDLER
+::    )
     
 echo Build successfull.
 exit /B 0
