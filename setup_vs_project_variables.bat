c1654d50c401daf936c7a61b4fbe487a5e04a09d
Alexey Ivanov <alexey0ivanov@gmail.com>
2016-08-02 02:22:28 +0300

    moving to visual studio 2015

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -4,7 +4,7 @@ call setup_environment.bat > nul
 
 set BOOST_DIR=c:\libraries\boost\boost_1_57_0
 set FREEIMAGELIB_DIR=%~dp0\3rd_party\FreeImage\%FreeImage_VERSION%
-set AIMP_PLUGINS_DIR=c:\AIMP\AIMP4.00.1680\Plugins
+set AIMP_PLUGINS_DIR=c:\AIMP\AIMP4.00.1695\Plugins
 ::for API 3.55- it should be
 ::  set AIMP_CONTROL_SUBPATH=\
 set AIMP_CONTROL_SUBPATH=\Control Plugin
--- vs_launch.bat
+++ vs_launch.bat
@@ -1,10 +1,4 @@
 @echo off
 call setup_vs_project_variables.bat > nul
-:: Visual Studio version is chosen by following line in sln file:
-:: # Visual C++ Express 2012
-rem start msvc\aimp_control_plugin.sln
 
-:: This starts Visual Studio 2013 even if aimp_control_plugin.sln points to Visual C++ Express 2010
-"%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\devenv" "%~dp0\msvc\aimp_control_plugin.sln"
-
-::start msvc\aimp_control_plugin.sln
+start msvc\aimp_control_plugin.sln
