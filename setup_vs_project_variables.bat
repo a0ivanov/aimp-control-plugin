e3c850acb4847e0464d89cda17ae72acdad685ec
Alexey Ivanov <alexey0ivanov@gmail.com>
2016-08-02 18:11:55 +0300

    upgrade boost from 1.57.0 to 1.61.0

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -2,7 +2,7 @@
 @echo off
 call setup_environment.bat > nul
 
-set BOOST_DIR=c:\libraries\boost\boost_1_57_0
+set BOOST_DIR=c:\libraries\boost\boost_1_61_0
 set FREEIMAGELIB_DIR=%~dp0\3rd_party\FreeImage\%FreeImage_VERSION%
 set AIMP_PLUGINS_DIR=c:\AIMP\AIMP4.00.1695\Plugins
 ::for API 3.55- it should be
