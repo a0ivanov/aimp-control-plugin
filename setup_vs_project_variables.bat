0a44b118d1fa1501642bad157d4f56341235e454
Alexey Ivanov <alexey0ivanov@gmail.com>
2016-08-12 01:00:28 +0300

    move to aimp 4.10.1827

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -4,7 +4,7 @@ call setup_environment.bat > nul
 
 set BOOST_DIR=c:\libraries\boost\boost_1_61_0
 set FREEIMAGELIB_DIR=%~dp0\3rd_party\FreeImage\%FreeImage_VERSION%
-set AIMP_PLUGINS_DIR=c:\AIMP\AIMP4.02.1725\Plugins
+set AIMP_PLUGINS_DIR=c:\AIMP\AIMP4.10.1827\Plugins
 ::for API 3.55- it should be
 ::  set AIMP_CONTROL_SUBPATH=\
 set AIMP_CONTROL_SUBPATH=\Control Plugin
