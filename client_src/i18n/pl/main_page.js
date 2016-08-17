948085253fec96370af675621030ee7f9e11cfb3
AviciiPL <deniswidera@gmail.com>
2016-08-17 09:14:06 +0200

    Add files via upload

---
+++ client_src/i18n/pl/main_page.js
@@ -0,0 +1,92 @@
+/*
+    File contains localization for AimpManager web-interface.
+    Copyright (c) 2012, Alexey Ivanov
+*/
+var language_main_page = {
+
+page_title : 'Remote AIMP player control',
+error_playlists_loading : 'Błąd pobierania playlisty',
+error_playlist_entries_loading : 'Błąd pobierania listy utworów playlisty',
+error_stop_playback : 'Błąd zatrzymywania utworu',
+error_pause_playback : 'Błąd wstrzymywania utworu',
+error_play_previous_track : 'Błąd odtwarzania poprzedniego utworu',
+error_play_next_track : 'Błąd odtwarzania następnego utworu',
+error_volume_get : 'Błąd pobierania ustawienia głośności',
+error_volume_set : 'Błąd ustawiania poziomu głośności',
+error_get_control_panel_state : 'Błąd pobierania ustawień programu AIMP Control Panel',
+
+reason : 'Powód',
+reason_unknown : 'Powód nieznany',
+playback_state_playing : 'Odtwarzanie',
+playback_state_stopped : 'Zatrzymane',
+kilobits_per_second : 'kbit/s',
+
+control_panel_playback : 'Odtwórz',
+control_panel_pause : 'Wstrzymaj',
+control_panel_stop : 'Zatrzymaj',
+control_panel_previous : 'Poprzedni utwór',
+control_panel_next : 'Następny utwór',
+control_panel_shuffle : 'Losowo',
+control_panel_shuffle_on : 'Włącz tryb losowego odtwarzania utworów',
+control_panel_shuffle_off : 'Wyłącz tryb losowego odtwarzania utworów',
+control_panel_repeat : 'Powtarzanie',
+control_panel_repeat_on : 'Włącz powtarzanie utworu',
+control_panel_repeat_off : 'Wyłącz powtarzanie utworu',
+control_panel_radio_capture_on: 'Włącz nagrywanie',
+control_panel_radio_capture_off: 'Wyłącz nagrywanie',
+control_panel_radio_capture_disabled: 'Nagrywanie jest dostępne tylko jeśli odtwarzasz radio',
+control_panel_settings : 'Ustawienia',
+control_panel_volume : 'Głośność',
+control_panel_mute : 'Wyciszanie',
+control_panel_mute_on : 'Wycisz',
+control_panel_mute_off : 'Wyłącz wyciszenie',
+
+settings_dialog_title : 'Ustawienia',
+settings_dialog_save_button : 'Zapisz ustawienia',
+settings_dialog_cancel_button : 'Anuluj',
+// tab 'playlist view'
+settings_dialog_playlist_view_tab_title : 'Wydok playlisty',
+settings_dialog_playlist_view_tab_show_fields : 'Pola wyświetlane',
+settings_dialog_playlist_view_tab_hide_fields : 'Pola ukryte',
+settings_dialog_playlist_view_tab_entry_field_warning : 'Musisz wybrać minimum jedno pole do wyświetlenia.',
+// tab 'language'
+settings_dialog_language_tab_title : 'Język',
+// tab track info
+settings_dialog_trackinfo_tab_title : 'Panel paska informacji',
+settings_dialog_trackinfo_format : 'Format paska informacji',
+
+entry_field_caption_title : 'Tytuł',
+entry_field_caption_artist : 'Artysta',
+entry_field_caption_album : 'Album',
+entry_field_caption_date : 'Rocznik',
+entry_field_caption_genre : 'Gatunek',
+entry_field_caption_bitrate : 'Bitrate',
+entry_field_caption_duration : 'Czas',
+entry_field_caption_filesize : 'Rozmiar',
+entry_field_caption_rating : 'Ocena',
+entry_field_caption_albumcover : 'Obrazek albumu',
+
+track_contol_menu_open : 'Rozwiń menu utworu',
+track_contol_menu_close : 'Zwiń menu utworu',
+track_contol_menu_play : 'Odwtórz',
+track_contol_menu_pause : 'Wstrzymaj',
+track_contol_menu_enqueue : 'Dodaj do kolejki',
+track_contol_menu_remove_from_queue : 'Usuń z kolejki',
+track_contol_menu_download_track : 'Pobierz',
+
+track_info_dialog_caption : 'Informacje o utworze',
+track_info_dialog_cancel_rating : 'Brak oceny',
+
+playlist_contol_menu_add : 'Dodaj pozycje do playlisty',
+playlist_contol_menu_add_disabled : 'Dodawanie pozycji do playlisty jest wyłączone w ustawieniach AIMP Control Plugin',
+playlist_contol_menu_add_not_supported : 'Dodawanie pozycji do playlisty nie jest obsługiwane. Proszę zaktualizować plugin.',
+playlist_contol_menu_item_add_file : 'Plik',
+playlist_contol_menu_item_add_url : 'URL',
+playlist_contol_dialog_file_add_title: 'Dodaj pliki',
+playlist_contol_dialog_file_add_message: 'Wybierz lub przeciągnij i upuść',
+playlist_contol_dialog_file_add_button_caption: 'Dodaj pliki',
+playlist_contol_dialog_url_add_title: 'Dodaj URL',
+playlist_contol_dialog_url_add_button_title: 'Dodaj',
+
+all_entries : 'Wszystko'
+}
